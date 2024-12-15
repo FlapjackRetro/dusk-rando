33ca65bff6eccf3eda8a345cbe4c69384754fd5a
Admiral Flapjack <admiral.flapjack@gmail.com>
2024-12-15 09:14:40 -0600

    Initial push

---
+++ DuskRando/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MelonLoader;
+using UnityEngine;
+
+namespace DuskRando
+{
+    public class MyFirstMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            //Here we will add our mod logic
+        }
+
+        private float lastTimeLog = 0;
+        private float logDelayInSecond = 1f;
+
+        public override void OnUpdate()
+        {
+            if (Time.time > lastTimeLog + logDelayInSecond)
+            {
+                lastTimeLog = Time.time;
+            }
+            else
+            {
+                return;
+            }
+
+            MelonLogger.Msg("Current temperature is: who cares");
+        }
+    }
+}
---
+++ DuskRando/ItemListJson.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuskRando
+{
+    public class ItemListJson
+    {
+        public static string ItemList = @"[
+            {
+                ""Reward"": {
+                    ""Name"": ""Pistol"",
+                    ""Type"": ""WEAPON_PICKUP"",
+                    ""Amount"": 1
+                },
+                ""Location"": {
+                    ""LocationId"": ""unknown"",
+                    ""SceneName"": ""E1M1"",
+                    ""SceneId"": 1,
+                    ""Position"": ""(-108.812, -255.65, 972.5)"",
+                    ""Requirements"": [
+                        {
+                            
+                        }
+                    ]
+                }
+            },
+            {
+                ""Reward"": {
+                    ""Name"": ""Pistol"",
+                    ""Type"": ""WEAPON_PICKUP"",
+                    ""Amount"": 1
+                },
+                ""Location"": {
+                    ""LocationId"": ""unknown"",
+                    ""SceneName"": ""E1M1"",
+                    ""SceneId"": 1,
+                    ""Position"": ""(-103.5, -256.9, 961)"",
+                    ""Requirements"": [
+                        {
+                            
+                        }
+                    ]
+                }
+            },
+            {
+                ""Reward"": {
+                    ""Name"": ""Pistol"",
+                    ""Type"": ""WEAPON_PICKUP"",
+                    ""Amount"": 1
+                },
+                ""Location"": {
+                    ""LocationId"": ""unknown"",
+                    ""SceneName"": ""E1M1"",
+                    ""SceneId"": 1,
+                    ""Position"": ""(-56.915, -246.4, 991.827)"",
+                    ""Requirements"": [
+                        {
+                            
+                        }
+                    ]
+                }
+            }
+        ]";
+    }
+}
